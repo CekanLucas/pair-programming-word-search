5e5c1a71d5ca0e8a423abd61d9d2f7f8e59f6c7c
CekanLucas <cekanlucas@gmail.coms>
2021-08-12 05:01:27 +0000

    all tests passing

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,10 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    // if ()
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    return false;
 }
 
 module.exports = wordSearch
